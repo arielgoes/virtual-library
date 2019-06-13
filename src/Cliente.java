26db5e352d609436cd207c20503c2c070b73ec19
lopesvictor1 <victorlopesvictor@outlook.com>
2019-06-13 10:09:37 -0300

    estoro de facao

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,4 +1,37 @@
 
 public class Cliente {
-
+	private String nome;
+	private int idade;
+	private String cpf;
+	
+	public Cliente(String nome, int idade, String cpf) {
+		this.nome = nome;
+		this.idade = idade;
+		this.cpf = cpf;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+	public String getNome() {
+		return this.nome;
+	}
+	
+	public int getIdade() {
+		return this.idade;
+	}
+	
+	public String getCpf() {
+		return this.cpf;
+	}
+	
 }
