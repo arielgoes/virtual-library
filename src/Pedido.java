830699809198fb6ca8b9cc8053cb0b114ed1e616
Ariel <arielgoescastro@live.com>
2019-06-15 11:22:05 -0300

    metodos do Pedido

--- src/Pedido.java
+++ src/Pedido.java
@@ -1,18 +1,26 @@
 import java.util.Date;
+import java.util.ArrayList;
 
 public class Pedido {
 	private String numeroPedido;
-	private Endereco enderecoEntrega;
+	private Endereco enderecoEntrega; //Classe 'Endereco'
 	private Date dataPedido;
 	private double valor;
+	private Status status; //Classe 'Status'
+	private Cliente cliente; //Classe 'Cliente'
+	private ArrayList<Livro> livros;
+	
 	
 	Pedido(String numeroPedido, Endereco enderecoEntrega, Date dataPedido, double valor){
 		this.numeroPedido = numeroPedido;
 		this.enderecoEntrega = enderecoEntrega;
 		this.dataPedido = dataPedido;
 		this.valor = valor;
+		this.livros = new ArrayList<Livro>();
 	}
 	
+	
+	//sets
 	void setNumeroPedido(String numeroPedido) {
 		this.numeroPedido = numeroPedido;
 	}
@@ -29,6 +37,16 @@ public class Pedido {
 		this.valor = valor;
 	}
 	
+	void setStatus(Status status) {
+		this.status = status;
+	}
+	
+	void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+	
+	
+	//gets
 	String getNumeroPedido() {
 		return this.numeroPedido;
 	}
@@ -45,4 +63,31 @@ public class Pedido {
 		return this.valor;
 	}
 	
+	Status getStatus() {
+		return this.status;
+	}
+	
+	Cliente getCliente() {
+		return this.cliente;
+	}
+	
+	
+	//funçoes
+	void addLivroPedido(Livro livro) {
+		this.livros.add(livro);
+	}
+	
+	
+	boolean delLivroPedido(String nomeLivro) {
+		for(Livro l: livros) {
+			if(l.getTitulo().equals(nomeLivro)) {
+				System.out.println("O livro " + nomeLivro + "foi excluído!");
+				livros.remove(l);
+				return true;
+			}
+		}
+		
+		System.out.println("O livro não foi encontrado e não pode ser removido!");
+		return false;
+	}	
 }
