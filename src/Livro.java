109bbaa8bc75cc644f25144ea5e68d015c2ff929
lopesvictor1 <victorlopesvictor@outlook.com>
2019-06-15 10:57:22 -0300

    Livro

--- src/Livro.java
+++ src/Livro.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Date;
 
 public class Livro {
@@ -10,7 +11,7 @@ public class Livro {
 	private double precoCusto;
 	private double margemLucro;
 	private int estoque = 0;
-	
+	private ArrayList<Autor> autores;
 	public Livro(String isbn, String titulo, String resumo, String material, Date dataPub, double preco, double precoCusto) {
 		
 		this.isbn = isbn;
@@ -21,7 +22,7 @@ public class Livro {
 		this.dataPub = dataPub;
 		this.precoCusto = precoCusto;
 		this.margemLucro = this.preco - this.precoCusto;
-		
+		this.autores = new ArrayList<Autor>();
 	}
 	
 	public void setIsbn(String isbn) {
@@ -76,6 +77,52 @@ public class Livro {
 	
 	public Date dataPub() {
 		return this.dataPub;
+	}
+	
+	public double getPrecoCusto() {
+		return this.precoCusto;
+	}
+	
+	public double getMargemLucro() {
+		return this.margemLucro;
+	}
+	
+	public int getEstoque() {
+		return this.estoque;
+	}
+	
+	public void addEstoque(int quant) {
+		this.estoque += quant;
+	}
+	
+	public void retirarEstoque(int quant) {
+		this.estoque += quant;
+	}
+	
+	public void addAutor(Autor a) {
+		this.autores.add(a);
+	}
+	
+	public boolean rmvAutor(String nome) {
+		int index = 0;
+		boolean found = false;
+		
+		for(Autor a: autores) {
+			index++;
+			if(a.getnomeAutor().equals(nome)) {
+				System.out.println("Removendo " + a.getnomeAutor() + " da lista de autores desse livro.");
+				found = true;
+				break;
+			}
+		}
+		if(found == true) {
+			autores.remove(index);
+			return true;
+		}else {
+			System.out.println("Autor não encontrado.");
+			return false;	
+		}
+		
 	}
 	
 }
