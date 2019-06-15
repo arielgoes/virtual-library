9fe652bd6ccb144f8c12c39f9e7b610e10e6eb46
Ariel <arielgoescastro@live.com>
2019-06-15 10:51:27 -0300

    Classe 'Pedido' (gets e sets)

--- src/Pedido.java
+++ src/Pedido.java
@@ -1,4 +1,48 @@
+import java.util.Date;
 
 public class Pedido {
-
+	private String numeroPedido;
+	private Endereco enderecoEntrega;
+	private Date dataPedido;
+	private double valor;
+	
+	Pedido(String numeroPedido, Endereco enderecoEntrega, Date dataPedido, double valor){
+		this.numeroPedido = numeroPedido;
+		this.enderecoEntrega = enderecoEntrega;
+		this.dataPedido = dataPedido;
+		this.valor = valor;
+	}
+	
+	void setNumeroPedido(String numeroPedido) {
+		this.numeroPedido = numeroPedido;
+	}
+	
+	void setEnderecoEntrega(Endereco enderecoEntrega) {
+		this.enderecoEntrega = enderecoEntrega;
+	}
+	
+	void setDataPedido(Date dataPedido) {
+		this.dataPedido = dataPedido;
+	}
+	
+	void setValor(double valor) {
+		this.valor = valor;
+	}
+	
+	String getNumeroPedido() {
+		return this.numeroPedido;
+	}
+	
+	Endereco getEnderecoEntrega() {
+		return this.enderecoEntrega;
+	}
+	
+	Date getDataPedido() {
+		return this.dataPedido;
+	}
+	
+	double getValor() {
+		return this.valor;
+	}
+	
 }
